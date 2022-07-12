375217e59082e674ea58a02cc8d15a87adb3e974
jilani-messaoud <35218288+jilani-messaoud@users.noreply.github.com>
2022-07-12 06:22:22 +0100

    Initial commit

---
+++ src/main/java/com/example/test/Service/EmailService.java
@@ -0,0 +1,17 @@
+package com.example.test.Service;
+
+// Importing required classes
+
+import com.example.test.Entity.EmailDetails;
+
+// Interface
+public interface EmailService {
+
+    // Method
+    // To send a simple email
+    String sendSimpleMail(EmailDetails details);
+
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment(EmailDetails details);
+}
